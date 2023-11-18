2fc3ed7e813caa3a5d6eb12956ea98c8dcc4e8f4
mykim-aus <doingmyk@gmail.com>
2023-11-18 23:32:05 +1100

    update test

--- test/server.js
+++ test/server.js
@@ -1,11 +1,13 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import GPTVAR from "gpt-var";
-// import "dotenv/config";
-const express = require("express");
-const bodyParser = require("body-parser");
-const GPTVAR = require("gpt-var");
-require("dotenv").config();
+import express from "express";
+import bodyParser from "body-parser";
+import GPTVAR from "gpt-var";
+import "dotenv/config";
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const GPTVAR = require("gpt-var");
+// console.log(GPTVAR);
+
+// require("dotenv").config();
 
 const app = express();
 const port = 3030;
@@ -20,22 +22,22 @@ app.get("/getArray", async (req, res) => {
 
     res.send(`OpenAI Response: ${JSON.stringify(response)}`);
   } catch (error) {
-    res.status(500).send("Error communicating with OpenAI");
+    res.status(500).send(error);
   }
 });
 
 app.get("/getJson", async (req, res) => {
   try {
-    const response = await openaiService.prompt(
-      `please dummy userData object which has "name" and "age" keys`,
-      "object"
+    const response = await gpt.prompt(
+      `please dummy user object which has "name" and "age" keys`,
+      "abc"
     );
 
     console.log(response);
 
     res.send(`OpenAI Response: ${JSON.stringify(response)}`);
   } catch (error) {
-    res.status(500).send("Error communicating with OpenAI");
+    res.status(500).send(error);
   }
 });
 
