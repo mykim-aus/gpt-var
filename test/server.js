76902c86ceda66033f5af32a13583c1ca9c35eb0
mykim-aus <doingmyk@gmail.com>
2023-11-19 00:16:26 +1100

    update

--- test/server.js
+++ test/server.js
@@ -1,11 +1,11 @@
-import express from "express";
-import bodyParser from "body-parser";
-import GPTVAR from "gpt-var";
-import "dotenv/config";
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const GPTVAR = require("gpt-var");
-// console.log(GPTVAR);
+// import express from "express";
+// import bodyParser from "body-parser";
+// import GPTVAR from "gpt-var";
+// import "dotenv/config";
+const express = require("express");
+const bodyParser = require("body-parser");
+const GPTVAR = require("gpt-var");
+console.log(GPTVAR);
 
 require("dotenv").config();
 
